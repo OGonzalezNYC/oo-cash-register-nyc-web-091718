0af661081dbc7827fc6fcd3262102d36eec9e989
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-09-19 01:07:37 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+  attr_accessor :total #:cash_register_with_discount
+  def initialize(cash_register_with_discount = 20)
+    @total = 0 
+ #   @cash_register_with_discount = cash_register_with_discount
+  end 
+  def total 
+    @total 
+  end 
+  def add_item(title, price, quantity = 1)
+    @total += price*quantity
+  end 
+  def apply_discount
+    @total 
+  end 
+end 
